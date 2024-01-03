78072d4c77868e39f75133db39609fd698633955
Alina Peshyk <freeshykoladka@gmail.com>
2024-01-03 16:23:28 +0100

    first migrate

--- my_internet_shop/catalog/models.py
+++ my_internet_shop/catalog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Catalog(models.Model):
+    name=models.CharField(max_length=255,unique=True)
+    order = models.PositiveSmallIntegerField()
+    is_visible = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = 'catalog'
+        ordering = ('order',)
+
+    
+    
+    def __str__(self):
+        return f' {self.name}'
+    
