6e90f7bb040cffa5f88603e5f473aaaa0b31352d
Alina Peshyk <freeshykoladka@gmail.com>
2024-01-30 17:43:51 +0100

    manager

--- app_main/models.py
+++ app_main/models.py
@@ -3,24 +3,24 @@ from django.urls import reverse
 from ckeditor.fields import RichTextField
 
 class CatalogItem(models.Model):
-    title=models.CharField(max_length=50, verbose_name='catalog item')
-    slug=models.SlugField(max_length=50, db_index=True,verbose_name='url')
-    url=models.CharField(max_length=100, blank= True)
-    is_anchor=models.BooleanField(default=False)
-    is_manage=models.BooleanField(default=True)
-    is_visible=models.BooleanField(default=True)
-    order=models.PositiveSmallIntegerField()
+    title = models.CharField(max_length=50, verbose_name='catalog item')
+    slug = models.SlugField(max_length=50, db_index=True, verbose_name='url')
+    url = models.CharField(max_length=100, blank=True)
+    is_anchor = models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
+    is_visible = models.BooleanField(default=True)
+    order = models.PositiveSmallIntegerField()
 
     def get_absolute_url(self):
         if self.is_anchor:
-            return reverse(f'shop:{self.slug}')
-        return reverse('shop:detail', kwargs={'slug': self.slug})
+            return reverse('shop:home') + f'#{self.slug}'
+        return self.slug
 
     def __str__(self):
-        return f' {self.title}/{self.slug}'
+        return f'{self.title}/{self.slug}'
 
     class Meta:
-        ordering=('order',)
+        ordering = ('order',)
 
 class Footer(models.Model):
     address = RichTextField()
